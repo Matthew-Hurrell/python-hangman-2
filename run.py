978a4d2046e2d9d53016327a712a1dd587b89339
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-19 09:09:53 +0000

    Add basic functionality to hangman game with list of words in list.py file

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from list import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives, "lives left. You have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives -= 1
+                print("Letter is not in the word.")
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again.")
+        else: 
+            print("Invalid character. Please try again.")
+    if lives == 0:
+        print("You died, sorry. The word was", word)
+    else:
+        print("You guessed the word", word, "!! Congratulations, you won!")
+
+hangman()
